390c4529fa3bf7d37fabe9759ee5d0f28da04038
wjdgofka <ttim1014@naver.com>
2020-03-07 13:09:08 +0900

    test

--- app/src/main/java/com/example/fitapp/MainActivity.java
+++ app/src/main/java/com/example/fitapp/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.fitapp;
 
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-public class MainActivity extends AppCompatActivity {
+    private SensorManager sensorManager;
+    private Sensor stepCountSensor;
+    TextView tvStepCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvStepCount = (TextView)findViewById(R.id.tvStepCount);
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(stepCountSensor == null) {
+            Toast.makeText(this, "No Step Detect Sensor", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, stepCountSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            tvStepCount.setText("Step Count : " + String.valueOf(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
 }
